11907ffbe511d4015cd1b263e02da7ae34e4db1d
Cobus Smit <cobus.smit@chillisoft.co.za>
2017-09-15 11:21:37 +0200

    feat: Greater and Less Than testing for DateTime types

--- src/NExpect.Tests/TestEqualityExtensions.cs
+++ src/NExpect.Tests/TestEqualityExtensions.cs
@@ -674,6 +674,7 @@ namespace NExpect.Tests
                     // Assert
                 }
             }
+            
         }
 
         [TestFixture]
@@ -1731,6 +1732,158 @@ namespace NExpect.Tests
                         Throws.Exception
                             .InstanceOf<UnmetExpectationException>()
                             .With.Message.Contains($"{(double) actual} to be less than {(double) expected}"));
+                    // Assert
+                }
+            }
+        }
+
+        [TestFixture]
+        public class ActingOnDateTimes
+        {
+            [TestFixture]
+            public class GreaterThan
+            {
+                [Test]
+                public void GreaterThan_WhenActualIsGreaterThanExpected_ShouldNotThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = GetRandomDate(null, actual);
+
+                    // Pre-Assert
+
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).To.Be.Greater.Than(expected);
+                    },
+                        Throws.Nothing);
+
+                    // Assert
+                }
+
+                [Test]
+                public void GreaterThan_WhenActualIsEqualToExpected_ShouldThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+
+                    // Pre-Assert
+
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).To.Be.Greater.Than(actual);
+                    },
+                        Throws.Exception.InstanceOf<UnmetExpectationException>());
+
+                    // Assert
+                }
+
+                [Test]
+                public void GreaterThan_WhenActualIsLessThanExpected_ShouldThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = GetRandomDate(actual, null);
+
+                    // Pre-Assert
+
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).To.Be.Greater.Than(expected);
+                    },
+                        Throws.Exception.InstanceOf<UnmetExpectationException>());
+
+                    // Assert
+                }
+
+                [Test]
+                public void GreaterThan_Negated_WhenActualIsGreaterThanExpected_ShouldNotThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = GetRandomDate(actual, null);
+                    // Pre-Assert
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).Not.To.Be.Greater.Than(expected);
+                    },
+                        Throws.Nothing);
+                    // Assert
+                }
+            }
+
+            [TestFixture]
+            public class LessThan
+            {
+                [Test]
+                public void LessThan_WhenActualIsLessThanExpected_ShouldNotThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = GetRandomDate(actual, null);
+                    // Pre-Assert
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).To.Be.Less.Than(expected);
+                    },
+                        Throws.Nothing);
+                    // Assert
+                }
+
+                [Test]
+                public void LessThan_WhenActualIsEqualToExpected_ShouldThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = actual;
+                    // Pre-Assert
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).To.Be.Less.Than(expected);
+                    },
+                        Throws.Exception
+                            .InstanceOf<UnmetExpectationException>()
+                            .With.Message.Contains($"{actual} to be less than {expected}"));
+                    // Assert
+                }
+
+                [Test]
+                public void LessThan_WhenActualIsGreaterThanExpected_ShouldThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = GetRandomDate(null, actual);
+                    // Pre-Assert
+                    // Act
+                    Assert.That(() =>
+                    {
+                        Expect(actual).To.Be.Less.Than(expected);
+                    },
+                        Throws.Exception
+                            .InstanceOf<UnmetExpectationException>()
+                            .With.Message.Contains($"{actual} to be less than {expected}"));
+                    // Assert
+                }
+
+                [Test]
+                public void LessThan_Negated_WhenActualIsGreaterThanExpected_ShouldNotThrow()
+                {
+                    // Arrange
+                    var actual = GetRandomDate();
+                    var expected = GetRandomDate(null, actual);
+                    // Pre-Assert
+                    // Act
+                    Assert.That(() =>
+                        {
+                            Expect(actual).Not.To.Be.Less.Than(expected);
+                        },
+                        Throws.Nothing);
                     // Assert
                 }
             }
