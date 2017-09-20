3a8f04062e1dcb761817f0fe134820a7c1051697
Cobus Smit <cobus.smit@chillisoft.co.za>
2017-09-20 09:29:55 +0200

    feat: Allow Key Value testing on plain Dictionaries using Expect(src).To.Contain.Key(key).With.Value(expected)

--- src/NExpect.Tests/Collections/DictionaryTesting.cs
+++ src/NExpect.Tests/Collections/DictionaryTesting.cs
@@ -5,6 +5,7 @@ using NExpect.Exceptions;
 using NExpect.Implementations;
 using static NExpect.Expectations;
 // ReSharper disable InconsistentNaming
+// ReSharper disable ExpressionIsAlwaysNull
 
 namespace NExpect.Tests.Collections
 {
@@ -17,6 +18,7 @@ namespace NExpect.Tests.Collections
             [TestFixture]
             public class Key
             {
+                [TestFixture]
                 public class OperatingOnPlainDictionary
                 {
                     [Test]
@@ -86,6 +88,259 @@ namespace NExpect.Tests.Collections
                                 ));
 
                         // Assert
+                    }
+                    
+                    [TestFixture]
+                    public class WithValue
+                    {
+                        [TestFixture]
+                        public class OperatingOnPrimitiveTypes
+                        {
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueMatches_ShouldNotThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                var value = GetRandomInt(2);
+                                var src = new Dictionary<string, int>()
+                                {
+                                    [key] = value
+                                };
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(key).With.Value(value);
+                                    },
+                                    Throws.Nothing);
+
+                                // Assert
+                            }
+
+                            [Test]
+                            public void WhenDictionaryDoesNotHaveKey_ShouldThrowForThatReason()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                var value = GetRandomInt(2);
+                                var src = new Dictionary<string, int>()
+                                {
+                                    [key] = value
+                                };
+                                var testingValue = GetAnother(key);
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(testingValue).With.Value(value);
+                                    },
+                                    Throws.Exception.TypeOf<UnmetExpectationException>()
+                                        .With.Message.Contains($"to contain key \"{testingValue}\""));
+
+                                // Assert
+                            }
+
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueDoesNotMatch_ShouldThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                var value = GetRandomInt(2);
+                                var src = new Dictionary<string, int>()
+                                {
+                                    [key] = value
+                                };
+                                var testingValue = GetAnother(value);
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(key).With.Value(testingValue);
+                                    },
+                                    Throws.Exception.TypeOf<UnmetExpectationException>()
+                                        .With.Message.Contains($"Expected {testingValue} but got {value}"));
+
+                                // Assert
+                            }
+                        }
+
+                        [TestFixture]
+                        public class OperatingOnNullable
+                        {
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueMatches_ShouldNotThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                int? value = GetRandomInt(2);
+                                var src = new Dictionary<string, int?>()
+                                {
+                                    [key] = value
+                                };
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(key).With.Value(value);
+                                    },
+                                    Throws.Nothing);
+
+                                // Assert
+                            }
+
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueDoesNotMatch_ShouldThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                int? value = GetRandomInt(2);
+                                var src = new Dictionary<string, int?>()
+                                {
+                                    [key] = value
+                                };
+                                var testingValue = GetAnother(value);
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(key).With.Value(testingValue);
+                                    },
+                                    Throws.Exception.TypeOf<UnmetExpectationException>()
+                                        .With.Message.Contains($"Expected {testingValue} but got {value}"));
+
+                                // Assert
+                            }
+
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueIsNull_ShouldThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                int? value = null;
+                                var src = new Dictionary<string, int?>()
+                                {
+                                    [key] = value
+                                };
+                                var testingValue = GetRandomInt();
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(key).With.Value(testingValue);
+                                    },
+                                    Throws.Exception.TypeOf<UnmetExpectationException>()
+                                        .With.Message.Contains($"Expected {testingValue} but got (null)"));
+
+                                // Assert
+                            }
+
+                            [Test]
+                            public void WhenDictionaryHasKey_AndMatchingValueIsNull_ShouldThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                int? value = GetRandomInt();
+                                var src = new Dictionary<string, int?>()
+                                {
+                                    [key] = value
+                                };
+                                int? testingValue = null;
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                    {
+                                        Expect(src).To.Contain.Key(key).With.Value(testingValue);
+                                    },
+                                    Throws.Exception.TypeOf<UnmetExpectationException>()
+                                        .With.Message.Contains($"Expected (null) but got {value}"));
+
+                                // Assert
+                            }
+                        }
+
+                        [TestFixture]
+                        public class OperatingOnEnumerable
+                        {
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueMatches_ShouldNotThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                var value = new List<int> {GetRandomInt(2,9), GetRandomInt(2, 9) , GetRandomInt(2, 9) };
+                                var src = new Dictionary<string, List<int>>()
+                                {
+                                    [key] = value
+                                };
+                                // Pre-Assert
+
+                                // Act
+                                Assert.That(() =>
+                                {
+                                    Expect(src).To.Contain.Key(key).With.Value(value);
+                                },
+                                    Throws.Nothing);
+
+                                // Assert
+                            }
+
+                            [Test]
+                            public void WhenDictionaryHasKey_AndValueDoesNotMatch_ShouldThrow()
+                            {
+                                // Arrange
+                                var key = GetRandomString(2);
+                                var value = new List<int> { GetRandomInt(2, 9), GetRandomInt(2, 9), GetRandomInt(2, 9) };
+                                var src = new Dictionary<string, List<int>>()
+                                {
+                                    [key] = value
+                                };
+                                var testingValue = new List<int> { GetRandomInt(10, 19), GetRandomInt(10, 19), GetRandomInt(10, 19) };
+                                // Pre-Assert
+                                
+                                // Act
+                                Assert.That(() =>
+                                {
+                                    Expect(src).To.Contain.Key(key).With.Value(testingValue);
+                                },
+                                    Throws.Exception.TypeOf<UnmetExpectationException>()
+                                        .With.Message.Contains($"Expected {testingValue} but got {value}"));
+
+                                // Assert
+                            }
+                        }
+
+                        //[TestFixture]
+                        //public class OperatingOnMisMatchedTypes
+                        //{
+                        //    [Test]
+                        //    public void WhenDictionaryValueIsByte_AndExpectedIsInt_ShouldThrow()
+                        //    {
+                        //        // Arrange
+                        //        var key = GetRandomString(2);
+                        //        var value = (sbyte)GetRandomInt(2);
+                        //        var src = new Dictionary<string, sbyte>()
+                        //        {
+                        //            [key] = value
+                        //        };
+                        //        var expected = (int)value;
+                        //        // Pre-Assert
+
+                        //        // Act
+                        //        Assert.That(() =>
+                        //        {
+                        //            //Expect(src).To.Contain.Key(key).With.Value(expected);
+                        //        },
+                        //            Throws.Nothing);
+
+                        //        // Assert
+                        //    }
+                        //}
                     }
                 }
             }
--- src/NExpect/DictionaryExtensions.cs
+++ src/NExpect/DictionaryExtensions.cs
@@ -11,7 +11,7 @@ namespace NExpect
     /// <summary>
     /// Provides matchers for dictionaries
     /// </summary>
-    public static class DictionaryExtensions 
+    public static class DictionaryExtensions
     {
         /// <summary>
         /// Tests if the provided collection contains the required key
@@ -20,14 +20,14 @@ namespace NExpect
         /// <param name="key">Key to look for</param>
         /// <typeparam name="TKey">Type of the dictionary keys</typeparam>
         /// <typeparam name="TValue">Type of the dictionary values</typeparam>
-        public static void Key<TKey, TValue>(
+        public static IDictionaryValueContinuation<TValue> Key<TKey, TValue>(
             this IContain<IEnumerable<KeyValuePair<TKey, TValue>>> continuation,
             TKey key
         )
         {
-            continuation.Key(key, null);
+            return continuation.Key(key, null);
         }
-
+        
         /// <summary>
         /// Tests if the provided collection contains the required key
         /// </summary>
@@ -36,7 +36,7 @@ namespace NExpect
         /// <param name="customMessage">Custom message to add to failure messages</param>
         /// <typeparam name="TKey">Type of the dictionary keys</typeparam>
         /// <typeparam name="TValue">Type of the dictionary values</typeparam>
-        public static void Key<TKey, TValue>(
+        public static IDictionaryValueContinuation<TValue> Key<TKey, TValue>(
             this IContain<IEnumerable<KeyValuePair<TKey, TValue>>> continuation,
             TKey key,
             string customMessage
@@ -53,6 +53,60 @@ namespace NExpect
                     )
                 );
             });
+
+            return CreateValueContinuation(continuation, key);
+        }
+        
+
+        /// <summary>
+        /// Tests if the provided key value matches the expected value
+        /// </summary>
+        /// <param name="continuation">Continuation to operate on</param>
+        /// <param name="expected">Value to match on</param>
+        /// <typeparam name="T">Type of the values</typeparam>
+        public static void Value<T>(
+            this IDictionaryValueWith<T> continuation,
+            T expected
+        )
+        {
+            continuation.Value(expected, null);
+        }
+
+        /// <summary>
+        /// Tests if the provided key value matches the expected value
+        /// </summary>
+        /// <param name="continuation">Continuation to operate on</param>
+        /// <param name="expected">Value to match on</param>
+        /// <param name="customMessage">Custom message to add to failure messages</param>
+        /// <typeparam name="T">Type of the values</typeparam>
+        public static void Value<T>(
+            this IDictionaryValueWith<T> continuation,
+            T expected,
+            string customMessage
+        )
+        {
+            continuation.AddMatcher(
+                EqualityProviderExtensions.GenerateEqualityMatcherFor(expected, customMessage)
+            );
+        }
+
+        private static IDictionaryValueContinuation<TValue> CreateValueContinuation<TKey, TValue>(IContain<IEnumerable<KeyValuePair<TKey, TValue>>> continuation, TKey key)
+        {
+            var continuationValue = GetValueForKey(continuation, key);
+            return Factory.Create<TValue, DictionaryValueContinuation<TValue>>(
+                continuationValue,
+                new WrappingContinuation<IEnumerable<KeyValuePair<TKey, TValue>>, TValue>(
+                    continuation, c => continuationValue
+                )
+            );
+        }
+
+        private static TValue GetValueForKey<TKey, TValue>(ICanAddMatcher<IEnumerable<KeyValuePair<TKey, TValue>>> continuation, TKey key)
+        {
+            return continuation
+                .GetActual()
+                .FirstOrDefault(kvp => kvp.Key.Equals(key))
+                .Value;
         }
     }
 }
