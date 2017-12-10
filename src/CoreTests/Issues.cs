675de0ede7c49a3c80fad1b47421c61e1dd5e7ff
Davyd McColl <davydm@gmail.com>
2017-12-10 17:05:46 +0200

    fix: types in dnc are enumerable, so Stringifying them ends up trying to stringify the entire app domain :/

---
+++ src/CoreTests/Issues.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using static NExpect.Expectations;
+using NExpect;
+using NExpect.Implementations;
+
+namespace CoreTests
+{
+    [TestFixture]
+    public class Issues
+    {
+        [Test]
+        public void ComparingTypes_ShouldNotStall()
+        {
+            // Arrange
+
+            // Pre-Assert
+
+            // Act
+            Assert.That(() =>
+            {
+                Expect(new Object().GetType()).To.Equal(typeof(Object));
+            }, Throws.Nothing);
+
+            // Assert
+        }
+
+        [Test]
+        public void StringifyObject()
+        {
+            // Arrange
+
+            // Pre-Assert
+
+            // Act
+            var result = (new object()).Stringify();
+
+            // Assert
+            Console.WriteLine(result);
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void StringifyType()
+        {
+            // Arrange
+
+            // Pre-Assert
+
+            // Act
+            var result = typeof(object).Stringify();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
---
+++ src/NExpect.Tests/Issues.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using static NExpect.Expectations;
+
+namespace NExpect.Tests
+{
+    [TestFixture]
+    public class Issues
+    {
+        [Test]
+        public void ComparingTypes_ShouldNotStall()
+        {
+            // Arrange
+
+            // Pre-Assert
+
+            // Act
+            Assert.That(() =>
+            {
+                Expect(new Object().GetType()).To.Equal(typeof(Object));
+            }, Throws.Nothing);
+
+            // Assert
+        }
+    }
+}
