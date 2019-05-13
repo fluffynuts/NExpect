cceff9b06757b10b56f7920466948ab91ec80957
Davyd McColl <davydm@gmail.com>
2019-05-13 14:44:57 +0200

    :sparkles: should use gulp to increment dependency versions too

--- local-tasks/increment-package-version.js
+++ local-tasks/increment-package-version.js
@@ -2,47 +2,69 @@ const gulp = requireModule("gulp-with-help"),
   gutil = require("gulp-util"),
   editXml = require("gulp-edit-xml");
 
-gulp.task(
-  "increment-package-version",
-  [
-    "increment-nexpect-package-version",
-    "increment-nexpect-nsubstitute-package-version"
-  ],
-  () => {
-    return Promise.resolve();
-  }
-);
-
-gulp.task("increment-nexpect-package-version", () => {
-  return incrementPackageVersionFor("NExpect");
-});
-
-gulp.task("increment-nexpect-nsubstitute-package-version", () => {
-  return incrementPackageVersionFor("NExpect.NSubstitute");
-});
-
-function incrementPackageVersionFor(project) {
-  const containingFolder = `src/${project}`;
+gulp.task("increment-package-version", () => {
   return gulp
-    .src(`${containingFolder}/Package.nuspec`)
-    .pipe(increment(project))
-    .pipe(gulp.dest(containingFolder));
-}
+    .src(["src/**/Package.nuspec", "!src/PeanutButter/**/*"])
+    .pipe(incrementPackageVersion())
+    .pipe(incrementDependencyVersion("NExpect"))
+    .pipe(gulp.dest("src"));
+});
 
-function increment(project) {
+function incrementDependencyVersion(packageMatch) {
+  if (typeof packageMatch === "string") {
+    packageMatch = new RegExp(`^${packageMatch}$`);
+  }
   return editXml(
     xml => {
-      const node = xml.package.metadata[0].version,
-        current = node[0],
-        parts = current.split("."),
-        major = parseInt(parts[0]),
-        minor = parseInt(parts[1]),
-        patch = parseInt(parts[2]);
-      testNaN({ major, minor, patch });
-      const newVersion = `${major}.${minor}.${patch + 1}`;
+      const meta = xml.package.metadata[0],
+        dependencies = meta.dependencies[0].group;
+      dependencies.forEach(dep => {
+        var dependency = (dep.dependency || [])[0];
+        if (!dependency) {
+          return;
+        }
+        if ((dependency.$.id || "").match(packageMatch)) {
+          const newVersion = incrementVersion(dependency.$.version);
+          gutil.log(
+            gutil.colors.yellow(
+              `${dependency.$.id}: dependency ${
+                dependency.$.id
+              } version incremented to: ${newVersion} from ${dependency.$.version}`
+            )
+          );
+          dependency.$.version = newVersion;
+        }
+      });
+      return xml;
+    },
+    {
+      builderOptions: { renderOpts: { pretty: true } }
+    }
+  );
+}
+
+function incrementVersion(versionString) {
+  const parts = versionString.split("."),
+    major = parseInt(parts[0]),
+    minor = parseInt(parts[1]),
+    patch = parseInt(parts[2]);
+  testNaN({ major, minor, patch });
+  return `${major}.${minor}.${patch + 1}`;
+}
+
+function incrementPackageVersion() {
+  return editXml(
+    xml => {
+      const meta = xml.package.metadata[0],
+        packageName = meta.id[0],
+        node = meta.version,
+        current = node[0];
+      const newVersion = incrementVersion(current);
       node[0] = newVersion;
       gutil.log(
-        gutil.colors.yellow(`${project}: package version incremented to: ${newVersion}`)
+        gutil.colors.yellow(
+          `${packageName}: package version incremented to: ${newVersion}`
+        )
       );
       return xml;
     },
