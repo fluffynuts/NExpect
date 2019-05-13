8b0d97056fc1611ce0918fc56d9abb5a1e57f881
Davyd McColl <davydm@gmail.com>
2019-05-13 15:56:15 +0200

    :sparkles: offload nuget package version / dep incrementing to gulp-tasks

--- local-tasks/increment-package-version.js
+++ local-tasks/increment-package-version.js
@@ -1,6 +1,7 @@
-const gulp = requireModule("gulp-with-help"),
-  gutil = require("gulp-util"),
-  editXml = require("gulp-edit-xml");
+const
+  gulp = requireModule("gulp-with-help"),
+  incrementPackageVersion = requireModule("gulp-increment-nuget-package-version"),
+  incrementDependencyVersion = requireModule("gulp-increment-nuget-package-dependency-version");
 
 gulp.task("increment-package-version", () => {
   return gulp
@@ -9,73 +10,3 @@ gulp.task("increment-package-version", () => {
     .pipe(incrementDependencyVersion("NExpect"))
     .pipe(gulp.dest("src"));
 });
-
-function incrementDependencyVersion(packageMatch) {
-  if (typeof packageMatch === "string") {
-    packageMatch = new RegExp(`^${packageMatch}$`);
-  }
-  return editXml(
-    xml => {
-      const meta = xml.package.metadata[0],
-        dependencies = meta.dependencies[0].group;
-      dependencies.forEach(dep => {
-        var dependency = (dep.dependency || [])[0];
-        if (!dependency) {
-          return;
-        }
-        if ((dependency.$.id || "").match(packageMatch)) {
-          const newVersion = incrementVersion(dependency.$.version);
-          gutil.log(
-            gutil.colors.yellow(
-              `${dependency.$.id}: dependency ${
-                dependency.$.id
-              } version incremented to: ${newVersion} from ${dependency.$.version}`
-            )
-          );
-          dependency.$.version = newVersion;
-        }
-      });
-      return xml;
-    },
-    {
-      builderOptions: { renderOpts: { pretty: true } }
-    }
-  );
-}
-
-function incrementVersion(versionString) {
-  const parts = versionString.split("."),
-    major = parseInt(parts[0]),
-    minor = parseInt(parts[1]),
-    patch = parseInt(parts[2]);
-  testNaN({ major, minor, patch });
-  return `${major}.${minor}.${patch + 1}`;
-}
-
-function incrementPackageVersion() {
-  return editXml(
-    xml => {
-      const meta = xml.package.metadata[0],
-        packageName = meta.id[0],
-        node = meta.version,
-        current = node[0];
-      const newVersion = incrementVersion(current);
-      node[0] = newVersion;
-      gutil.log(
-        gutil.colors.yellow(
-          `${packageName}: package version incremented to: ${newVersion}`
-        )
-      );
-      return xml;
-    },
-    { builderOptions: { renderOpts: { pretty: true } } }
-  );
-}
-
-function testNaN(version) {
-  Object.keys(version).forEach(k => {
-    if (isNaN(version[k])) {
-      throw new Error(`${k} is not an integer`);
-    }
-  });
-}
