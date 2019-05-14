cabebf767b5139c2c021a38e2c89c307c7cc8d4c
Davyd McColl <davydm@gmail.com>
2019-05-14 09:13:04 +0200

    :sparkles: offload git tagging and pushing functionality to gulp-tasks modules

--- local-tasks/tag-and-push.js
+++ local-tasks/tag-and-push.js
@@ -1,30 +1,14 @@
 const gulp = requireModule("gulp-with-help"),
   gutil = require("gulp-util"),
-  editXml = require("gulp-edit-xml"),
   Git = require("simple-git"),
   git = new Git(),
-  containingFolder = "src/NExpect";
+  gitPushTags = requireModule("git-push-tags"),
+  gitPush = requireModule("git-push"),
+  gitTagFromPackageNuspec = requireModule("gulp-git-tag-from-package-nuspec");
 
 gulp.task("tag-and-push", () => {
-  return new Promise((resolve, reject) => {
-    gulp.src(`${containingFolder}/Package.nuspec`).pipe(
-      editXml(xml => {
-        const node = xml.package.metadata[0].version,
-          version = node[0].trim();
-
-        gutil.log(gutil.colors.cyan(`Tagging at: "v${version}"`));
-        gitTag(`v${version}`, `chore(release): ${version}`)
-          .then(() => gitPushTags())
-          .then(() => gitPush())
-          .then(() => {
-            gutil.log(gutil.colors.green("-> all commits and tags pushed!"));
-            resolve();
-          })
-          .catch(err => reject(err));
-        return xml;
-      })
-    );
-  });
+  return gulp.src("**/NExpect/Package.nuspec")
+    .pipe(gitTagFromPackageNuspec());
 });
 
 gulp.task("push-tags", "Pushes tags and commits", () => {
@@ -34,38 +18,3 @@ gulp.task("push-tags", "Pushes tags and commits", () => {
       gutil.log(gutil.colors.green("-> all commits and tags pushed!"))
     );
 });
-
-function gitTag(tag, comment) {
-  return new Promise((resolve, reject) => {
-    git.addAnnotatedTag(tag, comment, err => {
-      if (err) {
-        return reject(err);
-      }
-      resolve();
-    });
-  });
-}
-
-function gitPushTags() {
-  return new Promise((resolve, reject) => {
-    gutil.log(gutil.colors.green("pushing tags..."));
-    git.pushTags("origin", err => {
-      if (err) {
-        return reject(err);
-      }
-      resolve();
-    });
-  });
-}
-
-function gitPush() {
-  return new Promise((resolve, reject) => {
-    gutil.log(gutil.colors.green("pushing local commits..."));
-    git.push("origin", "master", err => {
-      if (err) {
-        return reject(err);
-      }
-      resolve();
-    });
-  });
-}
