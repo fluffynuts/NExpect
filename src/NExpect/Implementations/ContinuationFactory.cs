0f355d2f897c6b2f19788954b33945fe86d584f6
Davyd McColl <davydm@gmail.com>
2020-03-25 16:48:15 +0200

    :fire: remove unused method

--- src/NExpect/Implementations/ContinuationFactory.cs
+++ src/NExpect/Implementations/ContinuationFactory.cs
@@ -8,19 +8,6 @@ namespace NExpect.Implementations
     /// </summary>
     public static class ContinuationFactory
     {
-        [Obsolete("Rather use the lazy eval version!")]
-        internal static T2 Create<T1, T2>(
-            T1 actual,
-            IExpectationContext<T1> parent,
-            Action<T2> afterConstruction = null
-        ) where T2 : IExpectationContext<T1>
-        {
-            var result = (T2) Activator.CreateInstance(typeof(T2), actual);
-            result.TypedParent = parent;
-            afterConstruction?.Invoke(result);
-            return result;
-        }
-        
         internal static T2 Create<T1, T2>(
             Func<T1> actualGenerator,
             IExpectationContext<T1> parent,
