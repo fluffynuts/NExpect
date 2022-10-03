4406db5ff4248bfdea10cc716f5f61bf0abf58a1
Davyd McColl <davydm@gmail.com>
2022-10-03 12:29:36 +0200

    :bug: should implement ICanAddMatcher<IEnumerable<T>> :|

--- src/NExpect/Interfaces/ICollectionA.cs
+++ src/NExpect/Interfaces/ICollectionA.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
+
 namespace NExpect.Interfaces
 {
     /// <summary>
     /// Continuation to provide the ".A" grammar for collections
     /// </summary>
     /// <typeparam name="T">Type of the continuation</typeparam>
-    public interface ICollectionA<T>: ICanAddMatcher<T>
+    public interface ICollectionA<T>: ICanAddMatcher<IEnumerable<T>>
     {
         /// <summary>
         /// Test if the provided actual collection is a superset of another
