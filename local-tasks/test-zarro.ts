70bef9cfa5d98de24e36fa0b72cd184c7e3835ff
Davyd McColl <davydm@gmail.com>
2023-09-08 09:04:58 +0200

    :wrench: support zarro testing

---
+++ local-tasks/test-zarro.ts
@@ -0,0 +1,14 @@
+/// <reference path="../node_modules/zarro/types.d.ts" />
+(function() {
+    const
+      gulp = requireModule<Gulp>("gulp");
+
+    gulp.task("test-zarro", async () => {
+        const
+          testZarro = requireModule<TestZarro>("test-zarro");
+        await testZarro({
+          package: "beta",
+          tasks: [ "test", "pack" ]
+        });
+    });
+})();
