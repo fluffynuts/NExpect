9a9fdbffd7946252da0dc8203db764bfc9ead407
Davyd McColl <davydm@gmail.com>
2024-06-19 10:03:36 +0200

    :sparkles: add ApproximateDateTimeEqualityComparer, to be used in deep equality testing
    GetHashCode won't return equal values for approximately-equal inputs, unfortunately
    - still need to have a think on this, but I don't think it's doable: imagine:
    
    we have 3 values: A, B, C
    we allow a 1s drift
    B - A = 750ms
    C - B = 750ms
    
    A ~ B (within 1s)
    C ~ B (within 1s)
    A !~ C (1.5s apart)

---
+++ src/NExpect.Tests/EqualityComparers/TestApproximateDateTimeEqualityComparer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using NExpect.EqualityComparers;
+using NUnit.Framework;
+
+namespace NExpect.Tests.EqualityComparers;
+
+[TestFixture]
+public class TestApproximateDateTimeEqualityComparer
+{
+    [TestFixture]
+    public class Equality
+    {
+        [TestFixture]
+        public class WhenDateTimesAreWithinConfiguredDrift
+        {
+            [Test]
+            public void ShouldReturnTrue()
+            {
+                // Arrange
+                var d1 = GetRandomDate();
+                var delta = GetRandomInt(250, 750);
+                var sign = GetRandomBoolean()
+                    ? -1
+                    : 1;
+                delta *= sign;
+                var d2 = d1.AddMilliseconds(delta);
+                var sut = Create(TimeSpan.FromSeconds(1));
+                // Act
+                var result = sut.Equals(d1, d2);
+                // Assert
+                Expect(result)
+                    .To.Be.True();
+            }
+        }
+
+        [TestFixture]
+        public class WhenDateTimesAreOutsideOfConfiguredDrift
+        {
+            [Test]
+            public void ShouldReturnFalse()
+            {
+                // Arrange
+                var d1 = GetRandomDate();
+                var delta = GetRandomInt(1250, 1750);
+                var sign = GetRandomBoolean()
+                    ? -1
+                    : 1;
+                delta *= sign;
+                var d2 = d1.AddMilliseconds(delta);
+                var sut = Create(TimeSpan.FromSeconds(1));
+                // Act
+                var result = sut.Equals(d1, d2);
+                // Assert
+                Expect(result)
+                    .To.Be.False();
+            }
+        }
+    }
+
+    private static ApproximateDateTimeEqualityComparer Create(
+        TimeSpan allowedDrift
+    )
+    {
+        return new(allowedDrift);
+    }
+}
---
+++ src/NExpect/EqualityComparers/ApproximateDateTimeEqualityComparer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NExpect.EqualityComparers
+{
+    /// <summary>
+    /// Provides an equality comparer to pass into
+    /// deep equality tests to allow for minor date/time
+    /// drifts, eg when data goes into, and then comes
+    /// back out of a database
+    /// </summary>
+    public class ApproximateDateTimeEqualityComparer: IEqualityComparer<DateTime>
+    {
+        private readonly double _allowableDriftMs;
+
+        /// <summary>
+        /// Construct the approximate comparer allowing 1s of drift
+        /// </summary>
+        public ApproximateDateTimeEqualityComparer() : this(1000)
+        {
+        }
+
+        /// <summary>
+        /// Construct the approximate comparer allowing the
+        /// configured number of ms drift
+        /// </summary>
+        /// <param name="allowedDriftMilliseconds"></param>
+        public ApproximateDateTimeEqualityComparer(int allowedDriftMilliseconds) : this(
+            TimeSpan.FromMilliseconds(allowedDriftMilliseconds)
+        )
+        {
+        }
+
+        /// <summary>
+        /// Construct the approximate comparer allowing the
+        /// configured timespan drift
+        /// </summary>
+        /// <param name="allowableDrift"></param>
+        public ApproximateDateTimeEqualityComparer(TimeSpan allowableDrift)
+        {
+            _allowableDriftMs = allowableDrift.TotalMilliseconds;
+        }
+
+        /// <inheritdoc />
+        public bool Equals(DateTime x, DateTime y)
+        {
+            var delta = Math.Abs((x - y).TotalMilliseconds);
+            return delta < _allowableDriftMs;
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(DateTime obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
